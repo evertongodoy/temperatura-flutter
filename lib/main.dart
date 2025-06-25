3b563d4e12b136a055a6f573df561eb083fb0e0c
Everton Godoy <everton.mgodoy@gmail.com>
2025-06-25 20:42:37 -0300

    final commit

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class TemperaturaApp extends StatelessWidget {
     return MaterialApp(
       title: 'Temperatura Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 229, 68, 9)),
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 227, 241, 26)),
       ),
       home: const TemperaturaScreen(title: 'Temperatura Senac'),
     );
@@ -31,6 +31,35 @@ class TemperaturaScreen extends StatefulWidget {
 
 class _TemperaturaScreenState extends State<TemperaturaScreen> {
   
+  final TextEditingController _temp = TextEditingController();
+  String _resultado = '';
+
+  double converter_para_fahrenheit(double celsius) {
+    return (celsius * 9 / 5) + 32;
+  }
+
+  double converter_para_kelvin(double celsius) {
+    return celsius + 273.15;
+  }
+
+  void _converter(String op) {
+
+    final temperatura = double.tryParse(_temp.text.replaceAll(',', '.'));
+    
+    if (temperatura == null) {
+      setState(() => _resultado = 'Entrada inválida');
+      return;
+    }
+
+    double res;
+    switch (op) {
+      case 'Fahrenheit': res = converter_para_fahrenheit(temperatura); break;
+      default: res = converter_para_kelvin(temperatura); break;
+    }
+
+    setState(() => _resultado = res.toString());
+  }  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,10 +67,29 @@ class _TemperaturaScreenState extends State<TemperaturaScreen> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Center(
-        
+      body: Padding(
+        padding: const EdgeInsets.all(16),
         child: Column(
-          
+          children: [
+            TextField(
+              controller: _temp,
+              keyboardType: TextInputType.numberWithOptions(decimal: true),
+              decoration: const InputDecoration(labelText: 'Digite uma Temperatura em Celsius'),
+            ),
+            const SizedBox(height: 16),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(onPressed: () => _converter('Fahrenheit'), child: const Text('Fahrenheit')),
+                ElevatedButton(onPressed: () => _converter('Kelvin'), child: const Text('Kelvin')),
+              ],
+            ),
+            const SizedBox(height: 24),
+            Text(
+              _resultado,
+              style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+          ],
         ),
       ),
     );
